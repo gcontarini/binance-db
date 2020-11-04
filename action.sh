bc62395d5f04701a9617b56e283c5cebada851ac
gcontarini <>
2020-11-04 18:34:39 -0300

    Change logic

--- action.sh
+++ action.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-# Go to home directory
-cd ~
+# Run script to all pairs
+for pair in $(cat tickers.txt) 
+do 
+	python3 todb_binance.py "$pair"
+	sleep 1
+done
 
-# First set up db server
-echo <password> | sudo -S -u postgres /Library/PostgreSQL/12/bin/pg_ctl -D /Library/PostgreSQL/12/data start
-
-# Activate env
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate quant
-
-# Run python script
-python ~/bin/download_bitcoin.py
-sleep 5
-python ~/bin/download_ether.py
-sleep 5
-python ~/bin/download_ripple.py
-sleep 5
-python ~/bin/download_binance.py
-
-
-# Stop db server
-echo facil | sudo -S -u postgres /Library/PostgreSQL/12/bin/pg_ctl -D /Library/PostgreSQL/12/data stop
+exit 0
